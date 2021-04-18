8e72de60db8a566326c2c0bcce17c4ca467ac74e
Keyvan Arj <keyvan@hamrahkish.com>
2021-04-18 14:22:23 +0430

    Java cryptolib is updated

--- src/java/cryptography-in-use/src/main/java/cryptolib/CryptoPdf.java
+++ src/java/cryptography-in-use/src/main/java/cryptolib/CryptoPdf.java
@@ -4,24 +4,7 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.encryption.AccessPermission;
 import org.apache.pdfbox.pdmodel.encryption.StandardProtectionPolicy;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.InvalidAlgorithmParameterException;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.SecretKeySpec;
-import javax.crypto.spec.IvParameterSpec;
-
-import java.util.Base64;
-
-public class CryptoPdf {
+public class CryptoPdf extends CryptoFile {
 
     public CryptoPdf() {
 
@@ -30,7 +13,6 @@ public class CryptoPdf {
     public Boolean encrypt(PDDocument plainDocument, 
                            String password, 
                            PDDocument cipheredDocument) {
-        
         try
         {
             //Creating access permission object
@@ -54,42 +36,6 @@ public class CryptoPdf {
             return true;
             
         } catch(Exception exception) {
-            return false;
-        }
-    }
-
-    public Boolean encrypt(File plainFile, 
-                           String keyBase64,
-                           String ivBase64, 
-                           File cipheredFile) {
-        
-        try
-        {
-            byte[] keyBytes = Base64.getDecoder().decode(keyBase64);
-            SecretKeySpec secretKeySpec = new SecretKeySpec(keyBytes, "AES");
-
-            byte[] ivBytes = Base64.getDecoder().decode(ivBase64);
-            IvParameterSpec ivParameterSpec = new IvParameterSpec(ivBytes);
-
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
-             
-            FileInputStream plainStream = new FileInputStream(plainFile);
-            byte[] plainBytes = new byte[(int) plainFile.length()];
-            plainStream.read(plainBytes);
-             
-            byte[] cipheredBytes = cipher.doFinal(plainBytes);
-             
-            FileOutputStream cipheredStream = new FileOutputStream(cipheredFile);
-            cipheredStream.write(cipheredBytes);
-             
-            plainStream.close();
-            cipheredStream.close();
-
-            return true;
-        } catch(NoSuchPaddingException | NoSuchAlgorithmException
-                | InvalidKeyException | BadPaddingException | InvalidAlgorithmParameterException
-                | IllegalBlockSizeException | IOException ex) {
             return false;
         }
     }
