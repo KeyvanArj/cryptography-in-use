c5c23856051c8682bccfd866618d5af44fae5b29
Keyvan Arj <keyvan@hamrahkish.com>
2021-06-02 10:09:28 +0430

    JS Secrecy unit tests are modified

--- src/js/cryptography-in-use/test/uinttest_cryptoobject.js
+++ src/js/cryptography-in-use/test/uinttest_cryptoobject.js
@@ -8,14 +8,14 @@ describe("Data/Message Secrecy", function() {
 
     const keyBase64 = 'QNV2GQxfFWXwmZTWmoJrSxYNLmqTUxv9g5NeQpabc7E=';
     const ivBase64 = '0eQbwlW32F6wbv7ca2n8bg==';
+    const plainBytes = FileStream.readFileSync(TestDataPath + '/bin/data_binary.bin');
 
     describe("Encryption", function() {
         it("AES-256 CBC Mode by PKCS5 Padding", function() {
             let cryptoObject = new CryptoObject();
-            let plainBytes = FileStream.readFileSync(TestDataPath + '/bin/data_binary.bin');
             let encryptedData = cryptoObject.encrypt_aes_256_cbc_pkcs5(plainBytes, keyBase64, ivBase64);
             let encryptedDataBase64 = encryptedData.toString('base64');
-            // openssl enc -aes-256-cbc -K 40d576190c5f1565f09994d69a826b4b160d2e6a93531bfd83935e42969b73b1 -iv d1e41bc255b7d85eb06efedc6b69fc6e -base64 -in bin/data_binary.bin
+            // $ openssl enc -aes-256-cbc -K 40d576190c5f1565f09994d69a826b4b160d2e6a93531bfd83935e42969b73b1 -iv d1e41bc255b7d85eb06efedc6b69fc6e -base64 -in bin/data_binary.bin
             Expect(encryptedDataBase64).to.equal('qxdVd8vkC6R1NBYi30q0wQ==')
         });
     });
@@ -23,11 +23,11 @@ describe("Data/Message Secrecy", function() {
     describe("Decryption", function() {
         it("AES-256 CBC Mode by PKCS5 Padding", function() {
             let cryptoObject = new CryptoObject();
+            // $ openssl enc -aes-256-cbc -K 40d576190c5f1565f09994d69a826b4b160d2e6a93531bfd83935e42969b73b1 -iv d1e41bc255b7d85eb06efedc6b69fc6e -in bin/data_binary.bin -out bin/encrypted_data.bin
             let encryptedBytes = FileStream.readFileSync(TestDataPath + '/bin/encrypted_data.bin');
             let decryptedData = cryptoObject.decrypt_aes_256_cbc_pkcs5(encryptedBytes, keyBase64, ivBase64);
-            let decryptedDataBase64 = decryptedData.toString('base64');
-            // openssl enc -aes-256-cbc -K 40d576190c5f1565f09994d69a826b4b160d2e6a93531bfd83935e42969b73b1 -iv d1e41bc255b7d85eb06efedc6b69fc6e -base64 -in bin/data_binary.bin
-            Expect(decryptedDataBase64).to.equal('/+I=')
+            // $ openssl enc -aes-256-cbc -K 40d576190c5f1565f09994d69a826b4b160d2e6a93531bfd83935e42969b73b1 -iv d1e41bc255b7d85eb06efedc6b69fc6e -base64 -in bin/data_binary.bin
+            Expect(decryptedData.toString('hex')).to.equal(plainBytes.toString('hex'))
         });
     });
 });
