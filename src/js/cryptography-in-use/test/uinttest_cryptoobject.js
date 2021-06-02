67851223b1cf7ce7218d5e59d929080edd1cc910
Keyvan Arj <keyvan@hamrahkish.com>
2021-06-02 10:53:41 +0430

    JS Secrecy unit tests are modified

--- src/js/cryptography-in-use/test/uinttest_cryptoobject.js
+++ src/js/cryptography-in-use/test/uinttest_cryptoobject.js
@@ -10,23 +10,29 @@ describe("Data/Message Secrecy", function() {
     const ivBase64 = '0eQbwlW32F6wbv7ca2n8bg==';
     const plainBytes = FileStream.readFileSync(TestDataPath + '/bin/data_binary.bin');
 
+    // To check the result of encryption by openssl, you can use the following commands :
+    // $ openssl enc -aes-256-cbc -K 40d576190c5f1565f09994d69a826b4b160d2e6a93531bfd83935e42969b73b1 -iv d1e41bc255b7d85eb06efedc6b69fc6e -in bin/data_binary.bin -out bin/encrypted_data.bin
+
+    // Or, to see the result in base64 encoding format : 
+    // $ openssl enc -aes-256-cbc -K 40d576190c5f1565f09994d69a826b4b160d2e6a93531bfd83935e42969b73b1 -iv d1e41bc255b7d85eb06efedc6b69fc6e -base64 -in bin/data_binary.bin
+
+    // For decryption also, you van use the following command : 
+    // $ openssl enc -d -aes-256-cbc -K 40d576190c5f1565f09994d69a826b4b160d2e6a93531bfd83935e42969b73b1 -iv d1e41bc255b7d85eb06efedc6b69fc6e -in bin/encrypted_data.bin -out bin/decrypted_data.bin
+
     describe("Encryption", function() {
         it("AES-256 CBC Mode by PKCS5 Padding", function() {
             let cryptoObject = new CryptoObject();
             let encryptedData = cryptoObject.encrypt_aes_256_cbc_pkcs5(plainBytes, keyBase64, ivBase64);
-            let encryptedDataBase64 = encryptedData.toString('base64');
-            // $ openssl enc -aes-256-cbc -K 40d576190c5f1565f09994d69a826b4b160d2e6a93531bfd83935e42969b73b1 -iv d1e41bc255b7d85eb06efedc6b69fc6e -base64 -in bin/data_binary.bin
-            Expect(encryptedDataBase64).to.equal('qxdVd8vkC6R1NBYi30q0wQ==')
+            let encryptedBytes = FileStream.readFileSync(TestDataPath + '/bin/encrypted_data.bin');
+            Expect(encryptedData.toString('hex')).to.equal(encryptedBytes.toString('hex'))
         });
     });
 
     describe("Decryption", function() {
         it("AES-256 CBC Mode by PKCS5 Padding", function() {
             let cryptoObject = new CryptoObject();
-            // $ openssl enc -aes-256-cbc -K 40d576190c5f1565f09994d69a826b4b160d2e6a93531bfd83935e42969b73b1 -iv d1e41bc255b7d85eb06efedc6b69fc6e -in bin/data_binary.bin -out bin/encrypted_data.bin
             let encryptedBytes = FileStream.readFileSync(TestDataPath + '/bin/encrypted_data.bin');
             let decryptedData = cryptoObject.decrypt_aes_256_cbc_pkcs5(encryptedBytes, keyBase64, ivBase64);
-            // $ openssl enc -aes-256-cbc -K 40d576190c5f1565f09994d69a826b4b160d2e6a93531bfd83935e42969b73b1 -iv d1e41bc255b7d85eb06efedc6b69fc6e -base64 -in bin/data_binary.bin
             Expect(decryptedData.toString('hex')).to.equal(plainBytes.toString('hex'))
         });
     });
